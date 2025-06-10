70446577d20e89fa87f0a45ba1c09a6c5df9fbb6
Mikhail <Migel.sorokin@yandex.ru>
2025-06-10 17:38:19 +0300

    Lab6 has been done (but secondary windows and icons doesn't work.

--- yota4/example-nestjs/src/main.ts
+++ yota4/example-nestjs/src/main.ts
@@ -5,16 +5,7 @@ import { resolve } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  
-  // Настройка CORS
-  app.enableCors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true,
-  });
 
-  // Настройка статических файлов
   app.useStaticAssets(resolve(__dirname, '..', 'public'));
 
   await app.listen(process.env.PORT ?? 3000);
